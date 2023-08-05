38b5e6958e3d28f63ff7434c66f1a7d26026abcd
Opliko <opliko.reg@protonmail.com>
2023-08-05 21:49:54 +0200

    Update plugins-installed.js

--- test/plugins-installed.js
+++ test/plugins-installed.js
@@ -6,8 +6,10 @@ const fs = require('fs');
 const db = require('./mocks/databasemock');
 
 const active = nconf.get('test_plugins') || [];
+console.log(active);
 const toTest = fs.readdirSync(path.join(__dirname, '../node_modules'))
 	.filter(p => p.startsWith('nodebb-') && active.includes(p));
+console.log(toTest);
 
 describe('Installed Plugins', () => {
 	toTest.forEach((plugin) => {
